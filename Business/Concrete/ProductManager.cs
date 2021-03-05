ad38dc4032c129d152b6db550a49cdc383f945c4
mervegundogmus <mervegundogmus@outook.com>
2021-03-05 08:10:48 +0300

    3 Şubat

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -17,6 +17,11 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        public List<Product> GetAllByCategoryId(int id) 
+        {
+            return _productDal.GetAll(p=>p.CategoryId==id);
+        }
+
         public List<Product> GetAll()
         {
             //İş kodları
@@ -24,6 +29,11 @@ namespace Business.Concrete
 
             return _productDal.GetAll();
 
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice <= min && p.UnitPrice <= max);
         }
     }
 }
