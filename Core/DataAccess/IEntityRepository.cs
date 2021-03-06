6099455029d64071957189fe3fb3eaa16fc46b1a
mervegundogmus <mervegundogmus@outook.com>
2021-03-06 03:28:18 +0300

    6 Şubat Mola

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,15 +1,15 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
-using System.Text;
 
-namespace DataAccess.Abstract
-{      
+namespace Core.DataAccess
+{
     //generic constraint
     //class : referans tip
     //IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir
     //new() : new'lenebilir olmalı
+
     public interface IEntityRepository<T> where T:class, IEntity, new() 
     {
         List<T> GetAll(Expression<Func<T,bool>> filter=null);
