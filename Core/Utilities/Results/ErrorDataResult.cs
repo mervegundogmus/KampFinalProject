a02ae06272d4b2669cfe166b718bf0a38890a075
mervegundogmus <mervegundogmus@outook.com>
2021-03-07 10:38:42 +0300

    10 şubat

--- Core/Utilities/Results/ErrorDataResult.cs
+++ Core/Utilities/Results/ErrorDataResult.cs
@@ -4,24 +4,24 @@ using System.Text;
 
 namespace Core.Utilities.Results
 {
-    public class ErrorDateResult<T> : DataResult<T>
+    public class ErrorDataResult<T>:DataResult<T>
     {
-        public ErrorDateResult(T data, string message) : base(data, false, message)
+        public ErrorDataResult(T data, string message) : base(data, false, message)
         {
 
         }
 
-        public ErrorDateResult(T data) : base(data, false)
+        public ErrorDataResult(T data) : base(data, false)
         {
 
         }
 
-        public ErrorDateResult(string message) : base(default, false, message)
+        public ErrorDataResult(string message) : base(default, false, message)
         {
 
         }
 
-        public ErrorDateResult() : base(default, false)
+        public ErrorDataResult() : base(default, false)
         {
 
         }
