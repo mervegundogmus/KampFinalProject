00e93ffa675295d6123abb3a1d9a7511e2aaeabf
mervegundogmus <mervegundogmus@outook.com>
2021-05-04 22:31:28 +0300

    27 Şubat Commit 1

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -29,7 +29,8 @@ namespace Business.Concrete
             _categoryService = categoryService;
         }
 
-
+        //Claim
+        //[SecuredOperation("product.add, admin")]
         [ValidationAspect(typeof(ProductValidator))]
 
         public IResult Add(Product product)
